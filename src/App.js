927a01f44f6eda8ef11876aa5be3396431b37fcf
Luis Z <lezv.dc@gmail.com>
2023-07-19 13:25:43 -0600

    Colors and React Router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { Outlet } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<main className="w-full h-full min-h-screen bg-bg-100">
+			Hello world!
+			<Outlet />
+		</main>
+	);
+};
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,31 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
+	darkMode: ['class', '[data-mode="dark"]'],
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: {
+					100: '#CFAE8E',
+					200: '#b19172',
+					300: '#6d5236',
+				},
+				accent: {
+					100: '#E8B98F',
+					200: '#835c36',
+				},
+				text: {
+					100: '#4D3830',
+					200: '#7a6259',
+				},
+				bg: {
+					100: '#F8E9D6',
+					200: '#eedfcc',
+					300: '#c5b7a5',
+				},
+			},
+		},
 	},
 	plugins: [],
 };
